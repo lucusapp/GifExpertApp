00b3cd12ccdf00aa523b8eefe338a13720282c5d
Lucusapp <lucusapp@gmail.com>
2021-01-01 17:14:00 +0100

    modificaciones antes de mostrar la aplicacion

---
+++ build/precache-manifest.a89e9611cfbef3531fe9ca182b971495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1718fedb74e4baf55d672fb5e32370de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4bd3f08ae213d8a21a",
+    "url": "/static/css/main.81f77afe.chunk.css"
+  },
+  {
+    "revision": "78227e003c389eec61e3",
+    "url": "/static/js/2.ee577153.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.ee577153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4bd3f08ae213d8a21a",
+    "url": "/static/js/main.5b80e635.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
